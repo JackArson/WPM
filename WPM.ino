a4c398cdc9b10008ba7d30b7dd28a94da243c2a8
Jack Arson <axe8765@gmail.com>
2019-03-11 11:53:26 -0400

    myTimer initial refactor

--- WPM.ino
+++ WPM.ino
@@ -7,32 +7,41 @@
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  
 class MyTimer
 {
-  // Class Member Variables
-  // These are initialized at startup
-  boolean double_digit;
-  
-  
-  // Constructor initializes the member variables and state
   public:
-  MyTimer()
-  {
-  }
+  MyTimer();
   byte counter;    // accessable from outside via (MyTimer).counter 
-  void Update() {
-    lcd.setCursor(8,0);
-    if(counter) {
-      if(counter <= 9) lcd.print(" "); // clear the first space if a single digit
-    lcd.print(counter);                 // print the counter
-    counter--;                          // counter decrements here
-    } else lcd.print("  ");             // no counter, so clear the board    
-  }
-  void Set(byte x) {
-
-    counter = x;
-  }
+  void Update       ();
+  void Set          (byte x); 
 };
 
+MyTimer::MyTimer()
+{}
 
+void MyTimer::Update()
+{
+    lcd.setCursor(8,0);
+    if(counter)
+    {
+        if(counter <= 9)
+        {
+            lcd.print(" "); // clear the first space if a single digit
+        }
+        lcd.print(counter);                 // print the counter
+        counter--;                          // counter decrements here
+    }
+    else
+    {
+        lcd.print("  ");             // no counter, so clear the board
+    }
+}
+
+void MyTimer::Set(byte x);
+{
+    counter = x;
+}
+
+
+  
 ////jon lynch apr 15 1968
 //                          
 //                          // Event type 1) B-day 2) Aniversary                                   
